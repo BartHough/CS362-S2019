3dff668da9d1c05a2576f7d69e0b962d8c213070
BartHough <39164910+BartHough@users.noreply.github.com>
2019-05-02 20:22:48 -0600

    up card2

---
+++ projects/houghjoh/dominion/cardtest2.c
@@ -0,0 +1,67 @@
+/* -----------------------------------------------------------------------
+ * Card test 2
+ * Testing the refactored "Sea_hag" card, located within cardEffect 
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+
+int main(){
+  
+  int newCards = 0;
+  int discarded = 1;
+  int xtraCoins = 0;
+  int shuffledCards = 0;
+  int i, j, m;
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+  int remove1, remove2;
+  int seed = 10;
+  int numPlayers = 2;
+  int thisPlayer = 0;
+  struct gameState G, testG;
+  int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,sea_hag, tribute, smithy, council_room};
+  // initialize a game state and player cards
+  initializeGame(numPlayers, k, seed, &G);
+  memcpy(&testG, &G, sizeof(struct gameState));
+  
+  
+   
+   
+  //Testing that discard increases by the number of players (2)
+  int discard=testG.discardCount[1];
+  discard+=2;
+  cardEffect(25, choice1, choice2, choice3, &testG, handpos, &bonus);
+  printf("\ntesting that discard increases\n");
+  printf("expected: %d ", discard);
+  printf("got: %d \n", testG.discardCount[1]);
+  if(discard == testG.discardCount[1]){
+    printf("PASSED\n");
+  }
+  else{
+    printf("FAILED\n");
+  }
+  
+  //Testing that discard increases by various numbers of players
+  printf("\ntesting that discard increases\n");
+  for(int i=3; i <10; i++){
+    initializeGame(i, k, seed, &G);
+    discard=G.discardCount[1];
+    discard+=i;
+    cardEffect(25, choice1, choice2, choice3, &G, handpos, &bonus);
+    printf("expected: %d ", discard);
+    printf("got: %d \n", G.discardCount[1]);
+    if(discard == G.discardCount[1]){
+      printf("PASSED\n");
+    }
+    else{
+      printf("FAILED\n");
+    }
+  }
+
+}
